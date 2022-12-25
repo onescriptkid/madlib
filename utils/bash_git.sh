29d8fa8f16659d8418a77199d4f92dfa484ca78e
onescriptkid <onescriptkid@gmail.com>
2022-12-24 16:36:40 -0800

    Fix bash_git template breaking scripts

--- utils/bash_git.sh
+++ utils/bash_git.sh
@@ -1,20 +1,12 @@
-  # Delightful git commands
-  GIT_REPO=$(git rev-parse --show-toplevel)
-  GIT_BRANCH=$(git rev-parse --abbrev-ref HEAD)
-  GIT_HASH=$(git rev-parse HEAD)
-  GIT_SHORT_HASH=$(git rev-parse --short HEAD)
+  ## Delightful git commands
+  GIT_REPO=$(git rev-parse --show-toplevel 2>/dev/null || printf "-")
+  GIT_BRANCH=$(git rev-parse --abbrev-ref HEAD 2>/dev/null || printf "-")
+  GIT_HASH=$(git rev-parse HEAD 2>/dev/null || printf "-")
+  GIT_SHORT_HASH=$(git rev-parse --short HEAD 2>/dev/null || printf "-")
 
-  # Grab the git version
-  if ! git describe --tags --abbrev=0 --match "v[0-9]*" > /dev/null 2>&1; then
-    GIT_VERSION=$(git describe --tags --abbrev=0 --match "v[0-9]*")
-  else
-    GIT_VERSION="v0.0.0"
-  fi
+  # Grab the git version. Assumes tags matching vX.X.X
+  GIT_VERSION=$(git describe --tags --abbrev=0 --match "v[0-9]*" 2>/dev/null || printf "-")
 
   # Grab the git-describe long version - <tag>-<#commits>-g<hash>
   # e.g. v1.0.1-1-g91ff3f3
-  if ! git describe --dirty --tags --long > /dev/null 2>&1; then
-    GIT_DESCRIBE=$(git describe --dirty --tags --long)
-  else
-    GIT_DESCRIBE="-"
-  fi
+  GIT_DESCRIBE=$(git describe --dirty --tags --long > /dev/null 2>/dev/null || printf "-")
